2caad6132d78dbd9c3fc714c9e76bc0e58439ba4
Jan Schnasse <schnasse@gmx.de>
2019-05-31 12:22:56 +0200

    add missing license and author information

--- app/helper/CrossrefLabelResolver.java
+++ app/helper/CrossrefLabelResolver.java
@@ -1,3 +1,20 @@
+/*Copyright (c) 2019 "hbz"
+
+This file is part of etikett.
+
+etikett is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package helper;
 
 import java.io.InputStream;
@@ -9,6 +26,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.google.common.io.CharStreams;
 
+/**
+ * @author Jan Schnasse
+ *
+ */
 public class CrossrefLabelResolver {
 
     final public static String id = "http://dx.doi.org/10.13039";
--- app/helper/SparqlLookup.java
+++ app/helper/SparqlLookup.java
@@ -1,3 +1,20 @@
+/*Copyright (c) 2019 "hbz"
+
+This file is part of etikett.
+
+etikett is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package helper;
 
 import java.net.URL;
@@ -13,6 +30,10 @@ import org.eclipse.rdf4j.query.TupleQueryResult;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.rio.RDFFormat;
 
+/**
+ * @author Jan Schnasse
+ *
+ */
 public class SparqlLookup {
 
     public static String lookup(String rdfAddress, String uri, String labelPredicate, String language, RDFFormat format,
