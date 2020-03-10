da203f333291fa552d296d9bc758dd6b5d9133e5
Andres Quast <quast@hbz-nrw.de>
2020-03-10 12:17:36 +0100

    refactored if clause

--- app/helper/EtikettMaker.java
+++ app/helper/EtikettMaker.java
@@ -250,24 +250,26 @@ public class EtikettMaker {
 
         try {
             Etikett result = getValue(urlAddress);
+
             if (result == null) {
                 result = getLabelFromUrlAddress(urlAddress);
-                if (result == null) {
+                if (result != null) {
                     addJsonDataIntoDBCache(result);
-                    play.Logger.debug("LookUp Label for the first time");
-                    return result;
-                } else if (result.getLabel().equals(result.getUri())
-                        && resolverProp.containsKey(url.getHost() + "/" + url.getPath())) {
-                    addJsonDataIntoDBCache(result);
-                    play.Logger.debug("LookUp Label because label equals URL");
-                    return result;
-                } else {
-                    play.Logger.debug("Fetch from db " + result + " " + result.getMultiLangSerialized());
                     return result;
                 }
+            } else if (result.getLabel().equals(result.getUri())
+                    && resolverProp.containsKey(url.getHost() + "/" + url.getPath())) {
+                result = getLabelFromUrlAddress(urlAddress);
+                if (result != null) {
+                    addJsonDataIntoDBCache(result);
+                    return result;
+                }
+            } else {
+                play.Logger.debug("Fetch from db " + result + " " + result.getMultiLangSerialized());
+                return result;
             }
         } catch (Exception e) {
-            play.Logger.warn(e.getMessage());
+            play.Logger.warn("", e);
         }
         Etikett result = new Etikett(urlAddress);
         result.label = urlAddress;
