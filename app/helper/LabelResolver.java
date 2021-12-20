d8a4834a796c0fe3ec0092476057b752dbca9660
Andres Quast <quast@hbz-nrw.de>
2021-12-20 09:04:44 +0100

    replace class SimpleResult with Result for migrate to a more recent play framework

--- app/helper/LabelResolver.java
+++ app/helper/LabelResolver.java
@@ -62,7 +62,7 @@ public interface LabelResolver {
             lResolver.put(PurlLabelResolver.DOMAIN, new PurlLabelResolver());
             lResolver.put(SkosLabelResolver.DOMAIN, new SkosLabelResolver());
             lResolver.put(EtikettMaker.TOSCIENCE_API_URL, new ToscienceApiLabelResolver());
-            lResolver.put(ResearchOrganízationsRegistry.DOMAIN, new ResearchOrganizationsLabelResolver());
+            lResolver.put(ResearchOrganizationLabelResolver.DOMAIN, new ResearchOrganizationLabelResolver());
 
             return lResolver;
         }
