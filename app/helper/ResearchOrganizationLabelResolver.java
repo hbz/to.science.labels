17f4b108628b9e4e4bf9f9e191c8059bd467cddf
Andres Quast <quast@hbz-nrw.de>
2021-12-20 09:52:06 +0100

    implement method lookup

--- app/helper/ResearchOrganizationLabelResolver.java
+++ app/helper/ResearchOrganizationLabelResolver.java
@@ -13,10 +13,23 @@ import java.util.Map;
 public class ResearchOrganizationLabelResolver extends LabelResolverService implements LabelResolver {
 
     public ResearchOrganizationLabelResolver() {
-        super();
     }
 
     public final static String DOMAIN = "ror.org";
+
+    public String lookup(String uri, String language) {
+        String label = null;
+        play.Logger.info("Search label for: " + uri);
+        GenericPropertiesLoader genProp = new GenericPropertiesLoader();
+        Map<String, String> map = genProp.loadVocabMap("ResearchOrganizationsRegistry-de.properties");
+        if (map.get(uri) != null) {
+            play.Logger.info("Found Label " + map.get(uri) + "for: " + uri);
+            label = map.get(uri);
+        } else {
+            label = "Testlauf";
+        }
+
+    }
 
     @Override
     protected void lookupAsync(String uri, String language) {
