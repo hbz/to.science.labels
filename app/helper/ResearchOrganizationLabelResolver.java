0d5b22cffeec3b7c47eed5958384e6b966cbda19
KayhanOgan <kayhan.ogan@hbz-nrw.de>
2022-09-19 16:18:00 +0200

    renamed properties files

--- app/helper/ResearchOrganizationLabelResolver.java
+++ app/helper/ResearchOrganizationLabelResolver.java
@@ -21,7 +21,7 @@ public class ResearchOrganizationLabelResolver extends LabelResolverService impl
         String label = null;
         play.Logger.info("Search label for: " + uri);
         GenericPropertiesLoader genProp = new GenericPropertiesLoader();
-        Map<String, String> map = genProp.loadVocabMap("ResearchOrganizationsRegistry-de.properties");
+        Map<String, String> map = genProp.loadVocabMap("affiliation-de.properties");
         if (map.get(uri) != null) {
             play.Logger.info("Found Label " + map.get(uri) + " for: " + uri);
             label = map.get(uri);
@@ -36,7 +36,7 @@ public class ResearchOrganizationLabelResolver extends LabelResolverService impl
     protected void lookupAsync(String uri, String language) {
         play.Logger.info("Search label for: " + uri);
         GenericPropertiesLoader genProp = new GenericPropertiesLoader();
-        Map<String, String> map = genProp.loadVocabMap("ResearchOrganizationsRegistry-de.properties");
+        Map<String, String> map = genProp.loadVocabMap("affiliation-de.properties");
         if (map.get(uri) != null) {
             play.Logger.info("Found Label " + map.get(uri) + " for: " + uri);
             etikett.setUri(uri);
