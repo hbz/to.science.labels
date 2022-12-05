4e950c4806aa9b34fc3a956e0a58174931f8f253
Ingolf Kuss <kuss@hbz-nrw.de>
2022-12-05 17:02:52 +0100

    Deploy- und Startskript für edoweb-test2 (SLES)

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if (( $EUID == 0 )); then
+    echo "Don't run as root!"
+    exit
+fi
+
+export TERM=xterm-color
+deployingApp="to.science.labels"
+# branch=$(git status | grep branch | cut -d ' ' -f3)
+# echo "git currently on branch: "$branch
+# if [ ! -z "$1" ]; then
+#     branch="$1"
+# fi
+
+cd /opt/toscience/src/$deployingApp
+#git pull origin $branch
+/opt/toscience/activator/activator dist
