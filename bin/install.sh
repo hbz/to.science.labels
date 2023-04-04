14dd15b922f0e984d69422a8837117c3659abe5c
Ingolf Kuss <kuss@hbz-nrw.de>
2023-04-04 21:19:00 +0200

    deploy.sh und install.sh und Start-Stop-Skript : Pfade geändert und in ein bin/-Verzeichnis geschoben

--- bin/install.sh
+++ bin/install.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
-## chanded by aquast, 13.09.2022
-
 if (( $EUID == 0 )); then
     echo "Don't run as root!"
     exit
 fi
 
 appDeployDir=$(pwd)
-deployDir="/opt/toscience/src"
+toscienceDir="/opt/toscience"
+deployDir="/opt/toscience/git"
 targetDir="/opt/toscience/apps"
-linkDir="to.science.labels"
+linkDir="toscience-labels"
 fileName="etikett-0.1.0-SNAPSHOT.zip"
 folderName="etikett-0.1.0-SNAPSHOT"
 newInstallDir="$linkDir.$(date +'%Y%m%d%H%M%S')"
-confDir="/etc/toscience/labels.conf"
+confDir="/etc/toscience/labels"
 
 cp $appDeployDir/target/universal/$fileName $deployDir
 
 cd $deployDir
 unzip $fileName
 
-# cp $targetDir/$linkDir/conf/application.conf $deployDir/$folderName/conf
-
 mv $deployDir/$folderName $targetDir/$newInstallDir
 mkdir $targetDir/$newInstallDir/logs
 
-if [ -L $targetDir/$linkDir ]; then
-       	rm $targetDir/$linkDir
+if [ -L $toscienceDir/$linkDir ]; then
+       	rm $toscienceDir/$linkDir
 fi
-ln -sf $targetDir/$newInstallDir $targetDir/$linkDir
+ln -sf $targetDir/$newInstallDir $toscienceDir/$linkDir
 rm -r  $targetDir/$newInstallDir/conf
 ln -sf $confDir $targetDir/$newInstallDir/conf
+
+echo ""
+echo "Neue Binärversion verfügbar unter $targetDir/$newInstallDir."
+echo "Port ist fest eingestellt auf: 9002"
+echo "Zum Umschalten auf die neue Version:"
+echo "sudo systemctl stop toscience-labels.service"
+echo "sudo systemctl start toscience-labels.service"
